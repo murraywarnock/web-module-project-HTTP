f9c8c0b6cca9e35c84ca0c8b53d210debeefc99f
MurrayWarnock <murray.warnock@gmail.com>
2021-07-21 13:35:32 -0600

    EditMovie.js working

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -6,6 +6,8 @@ import axios from 'axios';
 
 const EditMovieForm = (props) => {
 	const { push } = useHistory();
+	const { id } = useParams();
+	const { setMovies } = props;
 
 	const [movie, setMovie] = useState({
 		title:"",
@@ -14,6 +16,16 @@ const EditMovieForm = (props) => {
 		metascore: 0,
 		description: ""
 	});
+
+	useEffect(()=>{
+        axios.get(`http://localhost:5000/api/movies/${id}`)
+            .then(res=>{
+                setMovie(res.data);
+            })
+            .catch(err=>{
+                console.log(err.response);
+            })
+    }, [id]);
 	
 	const handleChange = (e) => {
         setMovie({
@@ -24,6 +36,14 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
 		e.preventDefault();
+		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
+		.then(res=> {
+		  setMovies(res.data);
+		  push(`/movies/${id}`);
+		})
+		.catch(err=> {
+		  console.log(err);
+		})
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
